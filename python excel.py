9d2638f208938a605d88b448b45cb884fab10f0f
ibrahimelsawy <ibrahim.sawy@gmail.com>
2017-03-03 19:12:40 +0200

    Add files via upload

---
+++ python excel.py
@@ -0,0 +1,40 @@
+from openpyxl import Workbook
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A3'] = datetime.datetime.now()
+
+# another method to assign value to cell by row & column address
+d = ws.cell(row=10, column=2, value=100)
+
+# Rename the sheet title
+ws.title = "Data"
+
+# Save the file
+wb.save("sample.xlsx")
+
+##########################################################################
+
+#You can also create new worksheets by using 
+ws = wb.create_sheet("first sheet", 0) # insert at first sheet tab
+ws = wb.create_sheet("last sheet") # insert at last sheet tab (default)
+# Save the file
+wb.save("sample.xlsx")
+
+##########################################################################
+#Review the names of all worksheets of the workbook
+print(wb.sheetnames)
+
+##########################################################################
+
+
