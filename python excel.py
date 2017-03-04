d1cdd07bf625890eeed461342b721e64c98882f4
ibrahimelsawy <ibrahim.sawy@gmail.com>
2017-03-05 01:25:28 +0200

    Update python excel.py

--- python excel.py
+++ python excel.py
@@ -37,4 +37,28 @@ print(wb.sheetnames)
 
 ##########################################################################
 
+# create new sheet and access column c
+ws = wb.create_sheet("accessing cell")
+colC = ws['C']
+print (colC)
+
+# access column range C:D
+col_range = ws['C:D']
+print (col_range)
+
+# access row 10
+row10 = ws[10]
+print(row10)
+
+# access row range 5:10
+row_range = ws[5:10]
+print (row_range)
+
+#obtain range of cells (rows & columns)
+cell_range = ws['A1':'C2']
+print (cell_range)
+
+wb.save("sample.xlsx")
+
+
 
